822581782e674ffd290ebdad9cc4415fef7e2ab4
ivan <ivanjv1234@gmail.com>
2024-06-10 20:13:46 -0600

    feat: user entity

--- src/data/mongo/models/user.model.ts
+++ src/data/mongo/models/user.model.ts
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Email is required']
     },
+    emailValidated: {
+        type: Boolean,
+        default: false
+    },
     password: {
         type: String,
         required: [true, 'Password is required']
