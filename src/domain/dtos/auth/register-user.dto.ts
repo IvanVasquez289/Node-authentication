da84d760e75e65d01c037ffdf206d86884adc476
ivan <ivanjv1234@gmail.com>
2024-06-11 23:25:58 -0600

    feat: register user dto

---
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -0,0 +1,19 @@
+import { regularExps } from "../../../config/regular-exp";
+
+export class RegisterUserDto {
+    constructor(
+        public name: string,
+        public email: string,
+        public password: string
+    ){}
+
+    public static create(object: {[key: string]: any}): [string?, RegisterUserDto?]{
+        const { name, email, password } = object;
+        if(!name) return ['name is required', undefined]
+        if(!email) return ['email is required', undefined]
+        if(!regularExps.email.test(email)) return ['email is not valid', undefined]
+        if(!password) return ['password is required', undefined]
+        if(password.length < 6) return ['password must have at least 6 characters', undefined]
+        return [undefined, new RegisterUserDto(name, email, password)]
+    }
+}
