7fdeba243ef154df7daebfb3816d2d3fb4f700da
ivan <ivanjv1234@gmail.com>
2024-06-28 21:44:54 -0600

    feat: pagination dto

---
+++ src/domain/dtos/shared/pagination.dto.ts
@@ -0,0 +1,13 @@
+export class PaginationDto {
+    private constructor(
+        public readonly page: number,
+        public readonly limit: number
+    ){}
+
+    static create(page: number, limit: number): [string?, PaginationDto?] {
+        if(isNaN(page) || isNaN(limit)) return ['page and limit must be numbers', undefined]
+        if(page <= 0) return ['page must be greater than 0', undefined]
+        if(limit <= 0) return ['limit must be greater than 0', undefined]
+        return [undefined, new PaginationDto(page, limit)]
+    }
+}
