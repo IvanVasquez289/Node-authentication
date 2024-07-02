c95d4f7b3a2662cf7074f4918d4d08b749507407
ivan <ivanjv1234@gmail.com>
2024-07-02 13:44:47 -0600

    feat: seeding db

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,9 +1,14 @@
 import { envs } from "../../config"
-import { MongoDataBase } from "../mongo"
+import { CategoryModel, MongoDataBase, ProductModel, UserModel } from "../mongo"
+import { seedData } from "./data"
 
 (async()=>{
     await main()
 })()
+
+const randomBetween0AndX = (x: number) => {
+    return Math.floor(Math.random() * x)
+}
 
 async function main(){
     await MongoDataBase.connect({
@@ -11,11 +16,36 @@ async function main(){
         dbName: envs.MONGO_DB_NAME
     })
    
+    // 0. Borrar todo
+    await Promise.all([
+        UserModel.deleteMany(),
+        CategoryModel.deleteMany(),
+        ProductModel.deleteMany()
+    ])
+
     // 1. Crear usuarios
+    const users = await UserModel.insertMany(seedData.users)
 
     // 2. Crear categorias
+    const categories = await CategoryModel.insertMany(
+        seedData.categories.map(category => {
+            return {
+                ...category,
+                user: users[0]._id
+            }
+        })
+    )
 
     // 3. Crear productos
+    const products = await ProductModel.insertMany(
+        seedData.products.map(product => {
+            return {
+                ...product,
+                user: users[randomBetween0AndX(users.length - 1)].id, 
+                category: categories[randomBetween0AndX(categories.length - 1)]._id
+            }
+        })
+    )
 
     await MongoDataBase.disconnect()
 }
