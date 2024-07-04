b131839a4f31658055b7dbd0aa466263d7f5dac9
ivan <ivanjv1234@gmail.com>
2024-07-04 17:22:02 -0600

    feat: asign sub folders

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -18,14 +18,18 @@ export class FileUploadController {
     }
 
     uploadFile = (req: Request, res: Response) => {
-        const files = req.files
+        const {type} = req.params
+        const validTypes = ['users', 'categories', 'products']
+
+        if(!validTypes.includes(type)) return res.status(400).json({ error: `Invalid type: ${type}, valid types: ${validTypes}` })
+
         if(!req.files || Object.keys(req.files).length === 0) {
             return res.status(400).json({ error: 'No files were selected' })
         }
 
         const file = req.files.file as UploadedFile
 
-        this.fileUploadService.uploadSingle(file)
+        this.fileUploadService.uploadSingle(file, `/uploads/${type}`)
             .then((data) => res.json(data))
             .catch((error) => this.handleError(error, res));
     }
